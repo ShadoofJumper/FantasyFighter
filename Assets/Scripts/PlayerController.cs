594eef366e8b99b02c7b4dd1b953f75a3bbd709c
ShadoofJumper <ShadoofJumper@gmail.com>
2020-05-25 15:16:11 +0300

    Complete main movement logic
    Complete main movement logic, add rotate camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,33 +9,54 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int attackDamage;
 
     private Rigidbody   playerRigidbody;
+    private Animator    playerAnimator;
 
     private Vector3 moveVelocity;
+    private bool    isAttack;
+    private bool    isMove;
+    private Vector3 rawMoveInput;
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator  = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        GetInput();
+        UpdateAnimation();
+        UpdateLookSide();
     }
 
 
     private void FixedUpdate()
     {
-        GetInput();
         MovePlayer();
+        Combat();
     }
 
     // --------------------- Movement logic ---------------------
     private void GetInput()
     {
-        moveVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        moveVelocity.Normalize();
+        rawMoveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        moveVelocity = CalculateMMoveVelocity(rawMoveInput);
+
+        isMove          = rawMoveInput.magnitude != 0 ? true : false;
+        isAttack        = Input.GetMouseButton(0) && !isMove;
+    }
+
+    private Vector3 CalculateMMoveVelocity(Vector3 input)
+    {
+        Vector3 globalForward   = Camera.main.transform.forward;
+        Vector3 globalRight     = Quaternion.Euler(0, 90, 0) * globalForward;
+
+        Vector3 verticalMovement    = globalForward * input.z;
+        Vector3 horizontalMovement  = globalRight * input.x;
+        return Vector3.Normalize(verticalMovement + horizontalMovement);
     }
 
     private void MovePlayer()
@@ -43,6 +64,35 @@ public class PlayerController : MonoBehaviour
         playerRigidbody.velocity = moveVelocity * moveSpeed;
     }
 
+    // --------------------- Combat logic -----------------------
+
+    private void Combat()
+    {
+        if (isAttack)
+        {
+            //Debug.Log("ATTACK!");
+        }
+    }
+
+
+    // -------------------- Animation logic ---------------------
+
+    private void UpdateAnimation()
+    {
+        float actualMoveSpeed = moveVelocity.magnitude;
+        playerAnimator.SetFloat("MoveSpeed", actualMoveSpeed);
+        if (isAttack)
+        {
+            //Debug.Log("Update attack anim!");
+            playerAnimator.SetTrigger("MainAttack");
+        }
+    }
+
+    private void UpdateLookSide()
+    {
+        if(rawMoveInput.x!=0)
+            transform.localScale = new Vector3(rawMoveInput.x, 1,1);
+    }
 
 
 }
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,19 +4,88 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-    private GameObject[] billboardSprites;
+    #region Singlton
 
-    // Start is called before the first frame update
+    public static SceneController instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("Try create another instance of game manager!");
+        }
+
+    }
+    #endregion
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private AnimationCurve rotateCurve;
+
+    private GameObject[] billboardSprites;
+    private float rotateStep = 90;
+    private float rotateTime = 0.5f;
+    private bool isRotate   = false;
+
+
     void Start()
     {
         ActivateBillboardsEffect();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        RotateWorld();
     }
+
+    private void RotateWorld()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            RotatePlayer(true);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RotatePlayer(false);
+        }
+    }
+
+    private void RotatePlayer(bool isClockWise)
+    {
+        if (isRotate)
+            return;
+
+        float angle = isClockWise ? rotateStep : rotateStep * -1;
+        Vector3 eulerAngleStep = new Vector3(0, angle, 0);
+
+        Quaternion startRotatetion = player.transform.rotation;
+        Quaternion finalRotation = Quaternion.Euler(player.transform.eulerAngles + eulerAngleStep);
+
+        StartCoroutine(RotateOverTime(startRotatetion, finalRotation, rotateTime));
+    }
+
+    IEnumerator RotateOverTime(Quaternion a, Quaternion b, float rTime)
+    {
+        isRotate = true;
+        float t     = 0.0f;
+        float step  = 1.0f / rTime;
+
+        while (t <= 1.0f)
+        {
+            t += Time.deltaTime * step;
+            float moveStep = rotateCurve.Evaluate(t);
+            player.transform.rotation = Quaternion.Slerp(a, b, moveStep);
+
+            yield return null;
+        }
+        isRotate = false;
+    }
+
 
 
     private void ActivateBillboardsEffect()
@@ -24,7 +93,6 @@ public class SceneController : MonoBehaviour
         billboardSprites = GameObject.FindGameObjectsWithTag("Billboard");
         foreach (GameObject spriteObject in billboardSprites)
         {
-            Debug.Log("bejct name: "+ spriteObject.name);
             spriteObject.AddComponent<Billboards>();
         }
     }
